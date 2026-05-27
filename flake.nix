d55c9dd113883869d7c5ba765eefbdb5184b326d
ben <koppe.development@gmail.com>
2026-05-27 12:26:15 -0700

    Fix nix flake after dependency updates

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
         {
           packages.default = pkgs.stdenv.mkDerivation (finalAttrs: {
             pname = "traforo";
-            version = "0.0.7";
+            version = "0.7.0";
 
             src = ./.;
 
@@ -40,7 +40,7 @@
               inherit (finalAttrs) pname version src;
               inherit pnpm;
               fetcherVersion = 3;
-              hash = "sha256-gknPoCnGgGE5+jInKXdO7KgHQDWUydGF+ulgpfmajTk=";
+              hash = "sha256-DjbGyg8Br7tTsaIVGM+Y74hBTftbRgVh1G/alDC881U=";
             };
 
             buildPhase = ''
@@ -105,93 +105,4 @@
         };
       flake = { };
     };
-  # flake-utils.lib.eachDefaultSystem (
-  #   system:
-  #   let
-  #     pkgs = import nixpkgs { inherit system; };
-  #     nodejs = pkgs.nodejs;
-  #     pnpm = pkgs.pnpm_10;
-  #   in
-  #   {
-  #     packages.default = pkgs.stdenv.mkDerivation (finalAttrs: {
-  #       pname = "traforo";
-  #       version = "0.0.7";
-  #
-  #       src = ./.;
-  #
-  #       nativeBuildInputs = [
-  #         nodejs
-  #         pnpm
-  #         pkgs.pnpmConfigHook
-  #         pkgs.makeWrapper
-  #       ];
-  #
-  #       pnpmDeps = pkgs.fetchPnpmDeps {
-  #         inherit (finalAttrs) pname version src;
-  #         pnpm = pnpm;
-  #         fetcherVersion = 3;
-  #         hash = "sha256-gknPoCnGgGE5+jInKXdO7KgHQDWUydGF+ulgpfmajTk=";
-  #       };
-  #
-  #       buildPhase = ''
-  #         runHook preBuild
-  #
-  #         pnpm build
-  #
-  #         runHook postBuild
-  #       '';
-  #
-  #       installPhase = ''
-  #         runHook preInstall
-  #
-  #         mkdir -p $out/lib/traforo $out/bin
-  #
-  #         cp -r dist $out/lib/traforo/
-  #         cp package.json $out/lib/traforo/
-  #
-  #         # Install production dependencies only
-  #         pnpm install --prod --frozen-lockfile --ignore-scripts
-  #         cp -r node_modules $out/lib/traforo/
-  #
-  #         makeWrapper ${nodejs}/bin/node $out/bin/traforo \
-  #           --add-flags "$out/lib/traforo/dist/cli.js"
-  #
-  #         runHook postInstall
-  #       '';
-  #
-  #       meta = {
-  #         description = "HTTP tunnel via Cloudflare Durable Objects and WebSockets";
-  #         license = pkgs.lib.licenses.mit;
-  #         mainProgram = "traforo";
-  #       };
-  #     });
-  #
-  #     apps.default = {
-  #       type = "app";
-  #       program = "${self.packages.${system}.default}/bin/traforo";
-  #     };
-  #
-  #     devShells.default = pkgs.mkShell {
-  #       buildInputs = [
-  #         nodejs
-  #         pnpm
-  #       ];
-  #
-  #       shellHook = ''
-  #         echo "traforo dev shell"
-  #         echo "  node $(node --version) | pnpm $(pnpm --version)"
-  #         echo ""
-  #         echo "Installing dependencies..."
-  #         pnpm install --frozen-lockfile 2>/dev/null || pnpm install
-  #         echo ""
-  #         echo "Available commands:"
-  #         echo "  pnpm dev          - Run Cloudflare worker locally (wrangler dev)"
-  #         echo "  pnpm deploy       - Deploy worker to Cloudflare"
-  #         echo "  pnpm build        - Build the CLI"
-  #         echo "  pnpm cli          - Run CLI from source (tsx)"
-  #         echo "  pnpm test         - Run tests"
-  #       '';
-  #     };
-  #   }
-  # );
 }
